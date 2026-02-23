4a68a5ffb2f46056aa5bb35d05575f09fab7cf7c
Muhammad Salman Iqbal <70254032+salmanjoyiaa@users.noreply.github.com>
2026-02-24 00:34:25 +0500

    Bypass Next.js aggressive cache on Supabase Client

--- src/lib/supabase/admin.ts
+++ src/lib/supabase/admin.ts
@@ -10,6 +10,11 @@ export function createAdminClient() {
         autoRefreshToken: false,
         persistSession: false,
       },
+      global: {
+        fetch: (url, options) => {
+          return fetch(url, { ...options, cache: "no-store" });
+        },
+      },
     }
   );
 }
